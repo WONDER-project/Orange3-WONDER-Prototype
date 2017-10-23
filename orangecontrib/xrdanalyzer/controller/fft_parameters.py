db181f12991d9294929f59a71f8553ecf5c9a047
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-10-23 15:26:13 +0200

    Business data structure

---
+++ orangecontrib/xrdanalyzer/controller/fft_parameters.py
@@ -0,0 +1,17 @@
+from orangecontrib.xrdanalyzer.util import congruence
+
+
+class FFTInitParameters:
+
+    s_max = 9.0
+    n_step = 4096
+
+    def __init__(self, s_max = 9.0, n_step = 4096):
+        congruence.checkStrictlyPositiveNumber(s_max, "S_max")
+        congruence.checkStrictlyPositiveNumber(n_step, "N_step")
+
+        if not ((n_step & (n_step - 1)) == 0) : raise ValueError("N_step should be a power of 2")
+
+        self.s_max = s_max
+        self.n_step = n_step
+
---
+++ orangecontrib/xrdanalyzer/controller/fit_initialization_parameters.py
@@ -0,0 +1,11 @@
+
+class FitInitializationParameters:
+
+    diffraction_pattern = None
+    crystal_structure = None
+    fft_parameters = None
+
+    def __init__(self, diffraction_pattern = None, crystal_structure = None, fft_parameters = None):
+        self.diffraction_pattern = diffraction_pattern
+        self.crystal_structure = crystal_structure
+        self.fft_parameters = fft_parameters
