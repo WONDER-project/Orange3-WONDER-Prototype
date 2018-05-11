306d0d461f95e00d23b06faba722931fefde16de
Luca Rebuffi <luca.rebuffi@elettra.eu>
2018-05-11 21:03:20 +0200

    Debug

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ NAME = 'Orange3-WONDER'
 
 MAJOR = 0
 MINOR = 0
-MICRO = 22
+MICRO = 25
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 AUTHOR = 'Luca Rebuffi, Paolo Scardi, Alberto Flor'
@@ -67,7 +67,46 @@ ENTRY_POINTS = {
 
 }
 
+import sys
+
+from distutils.core import setup
+from distutils.extension import Extension
+
+ext_modules=[
+    Extension("orangecontrib.xrdanalyzer.controller.fit.wppm_functions",               ["orangecontrib/xrdanalyzer/controller/fit/wppm_functions.pyx"]),
+    Extension("orangecontrib.xrdanalyzer.controller.fit.fitters.fitter_minpack",       ["orangecontrib/xrdanalyzer/controller/fit/fitters/fitter_minpack.pyx"]),
+    Extension("orangecontrib.xrdanalyzer.controller.fit.fitters.fitter_minpack_util",  ["orangecontrib/xrdanalyzer/controller/fit/fitters/fitter_minpack_util.pyx"]),
+]
+
+from Orange.canvas.application.addons import PipInstaller
+
+class Package:
+    def __init__(self, package_url="", name=""):
+        self.package_url = package_url
+        self.name = name
+
 if __name__ == '__main__':
+    pip = PipInstaller()
+
+    try:
+        if not sys.argv[1:][0] == "sdist": pip.upgrade(Package(package_url="pip", name="pip"))
+    except:
+        try:
+            pip.upgrade(Package(package_url="pip", name="pip"))
+        except:
+            pass
+
+    try:
+        from Cython.Distutils import build_ext
+    except:
+        try:
+            pip.arguments.append("--no-warn-script-location")
+            pip.install(Package(package_url="Cython"))
+        except:
+            pass
+
+    from Cython.Distutils import build_ext
+
     setup(
         name=NAME,
         version=VERSION,
@@ -84,4 +123,6 @@ if __name__ == '__main__':
         install_requires=INSTALL_REQUIRES,
         namespace_packages=['orangecontrib'],
         entry_points=ENTRY_POINTS,
+        cmdclass = {'build_ext': build_ext},
+        ext_modules = ext_modules,
     )
